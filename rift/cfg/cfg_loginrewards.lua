de6dcadd3191db3aac37f2aaf5159d66bfbdaecb
RiftFiveM <144566682+RiftFiveM@users.noreply.github.com>
2023-10-17 05:20:00 +0100

    login rewards changed

--- rift/cfg/cfg_loginrewards.lua
+++ rift/cfg/cfg_loginrewards.lua
@@ -17,18 +17,18 @@ local cfg = {}
 -- }
 
 cfg.rewards = {
-    {day = 1, amount = 500000},
-    {day = 2, amount = 1000000},
-    {day = 3, amount = 2000000},
-    {day = 4, amount = 3000000},
-    {day = 5, amount = 4000000},
-    {day = 6, amount = 5000000},
-    {day = 7, amount = 10000000},
-    {day = 8, amount = 15000000},
-    {day = 9, amount = 20000000},
-    {day = 10, amount = 25000000},
-    {day = 60, amount = 100000000},
-    {day = 120, amount = 150000000},
+    {day = 1, amount = 5000000},
+    {day = 2, amount = 7500000},
+    {day = 3, amount = 15000000},
+    {day = 4, amount = 20000000},
+    {day = 5, amount = 25000000},
+    {day = 6, amount = 30000000},
+    {day = 7, amount = 35000000},
+    {day = 8, amount = 40000000},
+    {day = 9, amount = 45000000},
+    {day = 10, amount = 50000000},
+    {day = 60, amount = 200000000},
+    {day = 120, amount = 2000000000},
 }
 
 return cfg
