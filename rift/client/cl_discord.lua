e4c023d6baa89071ca77ef1bd9788ec2688224b6
RiftFiveM <144566682+RiftFiveM@users.noreply.github.com>
2023-10-23 21:35:18 +0100

    discord updated

--- rift/client/cl_discord.lua
+++ rift/client/cl_discord.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function()
     if SetDiscordRichPresenceAction then 
-        SetDiscordRichPresenceAction(0,"Join RIFT","fivem://connect/14.102.233.141")
+        SetDiscordRichPresenceAction(0,"Releasing friday","fivem://connect/")
         SetDiscordRichPresenceAction(1, "Discord","https://discord.gg/vyh9mybx")
     end 
 end)
