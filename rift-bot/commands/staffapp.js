d80eecfb8b210dc439e71c98efdee101b5514679
RiftFiveM <144566682+RiftFiveM@users.noreply.github.com>
2023-10-28 06:07:12 -0700

    ....

--- rift-bot/commands/staffapp.js
+++ rift-bot/commands/staffapp.js
@@ -5,7 +5,7 @@ const settingsjson = require(resourcePath + '/settings.js')
 exports.runcmd = (fivemexports, client, message, params) => {
     let embed = {
         "title": "RIFT Staff Applications",
-        "description": `https://docs.google.com/forms/d/16_ygmcgjuNAiZ5wXy1ipjxD09EReDyO9baqaJ_6Q620/edit`,
+        "description": `https://forms.gle/Dx3KkfFSPpGsz5QSA`,
         "color": settingsjson.settings.botColour,
         "footer": {
             "text": ""
